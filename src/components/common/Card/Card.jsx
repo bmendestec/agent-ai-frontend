4aae632ca81feba8e368c5c40359f359bdcba512
Bruno Paula Mendes <bruno.paulamendes@philips.com>
2025-08-13 17:16:04 -0300

    update

---
+++ src/components/common/Card/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css';
+
+export default function Card({ title, children }) {
+    return (
+        <div className="card">
+            <h3>{title}</h3>
+            {children}
+        </div>
+    );
+}
--- src/pages/Dashboards.jsx
+++ src/pages/Dashboards.jsx
@@ -1,18 +1,56 @@
+import Card from '../components/common/Card/Card';
 import '../styles/Dashboards.css';
 
 export default function Dashboards() {
+    const mockCards = [
+        { title: 'Card 1' },
+        { title: 'Card 2' },
+        { title: 'Card 3' },
+        { title: 'Card 4' },
+    ]
+
     return (
         <>
             <div className="title">
                 <h1>Dashboards!</h1>
             </div>
             <h2>Main Dash</h2>
-            <div className="main-card">
-                <div className="card">
-                    <h3>card1</h3>
+            <div className="main-dash-container">
+                <div className="main-card">
+
+                    {mockCards.map((card) => (
+                        <Card title={card.title} />
+                    ))}
                 </div>
-                <div className="card">
-                    <h3>card1</h3>
+                <div className="main-card">
+
+                    {mockCards.map((card) => (
+                        <Card title={card.title} />
+                    ))}
+                </div>
+                <div className="main-card">
+
+                    {mockCards.map((card) => (
+                        <Card title={card.title} />
+                    ))}
+                </div>
+                <div className="main-card">
+
+                    {mockCards.map((card) => (
+                        <Card title={card.title} />
+                    ))}
+                </div>
+                <div className="main-card">
+
+                    {mockCards.map((card) => (
+                        <Card title={card.title} />
+                    ))}
+                </div>
+                <div className="main-card">
+
+                    {mockCards.map((card) => (
+                        <Card title={card.title} />
+                    ))}
                 </div>
             </div>
         </>
